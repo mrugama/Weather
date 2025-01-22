ea9589d7b7be1b85022fc574ecfbaaaa184cbcf3
Marlon Rugama <mrugama@gmail.com>
2025-01-22 12:06:21 -0500

    Minor update

--- WeatherCore/Sources/WeatherFeature/LandingPage/LandingPage.swift
+++ WeatherCore/Sources/WeatherFeature/LandingPage/LandingPage.swift
@@ -5,8 +5,12 @@ import SwiftUI
 @MainActor
 public struct LandingPage: Sendable {
     public static var pageView: some View {
-        lazy var dataLoader: DataLoader = ConcreteDataLoaderService().provideDataLoader()
-        lazy var restAPI: RestAPI = ConcreteRestAPIService().provideRestAPI(dataLoader)
-        return HomePage(viewModel: SearchViewModelImpl(restAPI))
+        HomePage(
+            viewModel: SearchViewModelImpl(
+                ConcreteRestAPIService().provideRestAPI(
+                    ConcreteDataLoaderService().provideDataLoader()
+                )
+            )
+        )
     }
 }
