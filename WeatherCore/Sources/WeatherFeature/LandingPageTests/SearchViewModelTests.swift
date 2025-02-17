3980afa15fac825faf51693e764ed1f64387fe6e
Marlon Rugama <mrugama@gmail.com>
2025-02-17 11:48:27 -0500

    Add test coverage to Landing Page - Model

--- WeatherCore/Sources/WeatherFeature/LandingPageTests/SearchViewModelTests.swift
+++ WeatherCore/Sources/WeatherFeature/LandingPageTests/SearchViewModelTests.swift
@@ -1,9 +1,9 @@
 import Foundation
-@testable import NetworkingTestUtilities
-@testable import RestAPI
-@testable import RestAPITestUtilities
-import Testing
+import NetworkingTestUtilities
+import RestAPI
+import RestAPITestUtilities
 @testable import LandingPage
+import Testing
 
 @Suite("Weather ViewModel")
 @MainActor
@@ -31,10 +31,20 @@ struct WeatherViewModelTests {
         dataLoader.dataToReturn = provideMockWeatherData()
         
         // When
-        let model = await viewModel.fetchWeatherData(from: "Manhattan")
+        let model: WeatherModel? = await viewModel.fetchWeatherData(from: "Manhattan")
         
         // Then
         #expect(model?.name == "Manhattan")
+        #expect(model?.tempCFormatted == "-4")
+        #expect(model?.tempFFormatted == "25")
+        #expect(model?.feelsLikeCFormatted == "-9")
+        #expect(model?.feelsLikeFFormatted == "15")
+        #expect(model?.tempSymbol == "°")
+        #expect(model?.humidityLabel == "Humidity")
+        #expect(model?.uvLabel == "UV")
+        #expect(model?.feelsLikeLabel == "Feels like")
+        #expect(model?.icon64x64URL.absoluteString == "https://cdn.weatherapi.com/weather/64x64/day/113.png")
+        #expect(model?.icon128x128URL.absoluteString == "https://cdn.weatherapi.com/weather/128x128/day/113.png")
         #expect(viewModel.appState != .somethingWentWrong(error: "Unable to fetch data"))
     }
     
